e38e753e655465a29676422ab412a902e0071e39
Oleg Pedorenko <olezhkoo@gmail.com>
2016-02-27 23:01:02 +0200

    Task3. Fixed the abitility to run applications via command line arguments

--- sandboxedModule/en/framework.js
+++ sandboxedModule/en/framework.js
@@ -6,7 +6,27 @@
 var fs = require('fs'),
     vm = require('vm'),
 	util = require('util');
-
+	
+var sandboxFactory = {
+	// Create a hash and turn it into the sandboxed context which will be
+	// the global context of an application
+	createSandbox: function(){
+		var context = { 
+				module: {}, 
+				console: console, 
+				setTimeout: function(callback, timeout) {
+					console.log("Function setTimeout was called with interval " + timeout + "ms");
+					setTimeout(callback, timeout);
+				}, 
+				setInterval: setInterval,
+				clearInterval: clearInterval,
+				util: util
+			};
+		context.global = context;		
+		var sandbox = vm.createContext(context);
+		return sandbox;
+	}		
+}
 
 process.argc = process.argv.length;
 // Print an error message if there are no parameters. First two params are node and framework.
@@ -17,37 +37,17 @@ if(process.argv.length < 3)
 else
 {
 	//Read and run every file in command line argument
-	process.argv.shift();
-	process.argv.shift();
-	console.dir(process.argv);
-	process.argv.forEach(function(item, id) {		
-		// Create a hash and turn it into the sandboxed context which will be
-		// the global context of an application
-		var context = 
-			{ 
-				module: {}, 
-				console: console, 
-				setTimeout: 
-				function(callback, timeout) {
-					console.log("Function setTimeout was called with interval " + timeout + "ms");
-					setTimeout(callback, timeout);
-				}, 
-				setInterval: setInterval,
-				clearInterval: clearInterval,
-				util: util
-			};
-		context.global = context;		
-		var sandbox = vm.createContext(context);
-		
+	process.argv.slice(2).forEach(function(item, id) {
 		// Read an application source code from the file
 		var fileName = './' + item + '.js';
 		fs.readFile(fileName, function(err, src) {
 			// We need to handle errors here
 			if (err) {
-				console.log("File not found");
+				console.log("File " + item + " not found");
 			} else {
 				// Run an application in sandboxed context
 				var script = vm.createScript(src, fileName);
+				var sandbox = sandboxFactory.createSandbox();
 				script.runInNewContext(sandbox);
 				// We can access a link to exported interface from sandbox.module.exports
 				// to execute, save to the cache, print to console, etc.
