03cece93161c3d13f3a26004d7a14cca610a7dd1
Oleg Pedorenko <olezhkoo@gmail.com>
2016-03-09 11:09:21 +0200

    Removed timer-related code from application

--- interfaceWrapper/en/application.js
+++ interfaceWrapper/en/application.js
@@ -1,14 +1,6 @@
 // Print something
 console.log('From application global context');
 
-// Declare function for timer event
-function timerEvent() {
-  console.log('From application timer event');
-}
-
-// Create timer
-setTimeout(timerEvent, 1000);
-
 // Example for fs API
 var fileName = './README.md';
 console.log('Application going to read ' + fileName);
